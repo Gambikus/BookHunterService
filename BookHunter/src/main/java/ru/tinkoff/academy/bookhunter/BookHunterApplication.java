04639f8d455fbe25638d180ad6fa01746be489ab
Vasya Veretin <ver4vas@ya.ru>
2022-10-13 22:15:56 +0300

    init

---
+++ BookHunter/src/main/java/ru/tinkoff/academy/bookhunter/BookHunterApplication.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.academy.bookhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookHunterApplication.class, args);
+    }
+
+}
